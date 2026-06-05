df505d8321531322addffb5dd0bbc237752b5779
n1koiyatelegram-blip <n1koiyatelegram@gmail.com>
2026-06-05 07:00:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "8045822374:AAFPvLyjwCdPndVDomcN_plp-_mhxkHgIww"
+TOKEN = "8045822374:AAGNjQ3gUDk6B5ZAwU8uHOlt2Ixx99LMnDI"
 ADMIN_ID = 8561804900  # ваш ID
 
 # Команда /test – отвечает в любом чате (даже без проверки админа)
